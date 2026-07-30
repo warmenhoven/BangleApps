3528b8e78c934ee50b7f4b0706b014490b687760
RKBoss6 <kabironkar@gmail.com>
2026-07-30 10:12:30 -0400

    Update haptic feedback call with 'touch' parameter

--- apps/launch/app.js
+++ apps/launch/app.js
@@ -49,7 +49,7 @@
       select : i => {
         var app = apps[i];
         if (!app) return;
-        if (Bangle.haptic) Bangle.haptic();
+        if (Bangle.haptic) Bangle.haptic("touch");
         if (!app.src || require("Storage").read(app.src)===undefined) {
           E.showScroller();
           E.showMessage(/*LANG*/"App Source\nNot found");
