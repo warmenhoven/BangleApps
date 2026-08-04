32c9b5fc0465da60338e27967dcbb9ba659e29bd
RKBoss6 <kabironkar@gmail.com>
2026-08-03 23:14:22 -0400

    Added setting to show/hide percent value

---
+++ apps/applebatt/settings.js
@@ -0,0 +1,28 @@
+(function(back) {
+  const FILE = "applebatt.settings.json";
+  // Load settings
+  var settings = Object.assign({
+    showPercent: true,
+  }, require('Storage').readJSON(FILE, true) || {});
+
+  function writeSettings() {
+    require('Storage').writeJSON(FILE, settings);
+  }
+
+
+  // Show the menu
+  E.showMenu({
+    "" : { "title" : "Apple Battery" },
+    "< Back" : () => back(),
+    'Show Percent': {
+      value: !!settings.showPercent,  // !! converts undefined to false
+      onchange: v => {
+        settings.showPercent = v;
+        writeSettings();
+        if(WIDGETS["applebatt"]) WIDGETS["applebatt"].reload()
+        if(WIDGETS["applebatt"]) WIDGETS["applebatt"].draw()
+
+      }
+    }
+  });
+})
