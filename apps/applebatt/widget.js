6cb202326f08749166306c7ad19b7c94158c4286
RKBoss6 <kabironkar@gmail.com>
2026-08-03 23:14:22 -0400

    Added setting to show/hide percent value

--- apps/applebatt/widget.js
+++ apps/applebatt/widget.js
@@ -1,29 +1,31 @@
 (function(){
   const intervalLow = 120000;
   const intervalHigh = 30000;
-
-
+  var id;
+  let settings;
+  function reload() {
+    settings=require("Storage").readJSON("applebatt.settings.json")||{showPercent:true}
+  }
   function draw() {
+    g.reset("widget");
     var x = this.x, y = this.y, w = this.width-5;
     const l = E.getBattery();
     const lw=(w)*(l/100)
-
     g.setColor("#808080");
     g.fillRect({x:x+2,y:y+2,w:w,h:16,r:6})
     let col=g.theme.fg
     if(l<20) col="#f00"
     if(Bangle.isCharging()) col="#0f0"
-    g.setColor(col);
-    g.fillRect({x:x+2,y:y+2,w:lw,h:16,r:6})
-    g.setFontAlign(0,0).setColor(g.theme.bg)
-    g.setFont("14").drawString(l,x+2+(w/2),y+12)
+    g.setColor(col)
+      .fillRect({x:x+2,y:y+2,w:lw,h:16,r:6})
+      .setFontAlign(0,0).setColor(g.theme.bg)
+    if(settings.showPercent) g.setFont("14").drawString(l,x+2+(w/2),y+12)
     if (Bangle.isCharging()) changeInterval(id, intervalHigh);
-    else changeInterval(id, intervalLow);
+      else changeInterval(id, intervalLow);
   }
-
-
-  WIDGETS["applebatt"] = { area:"tr", width:32, draw:draw };
-  Bangle.on('charging', () => WIDGETS["applebatt"].draw());
-  var id = setInterval(() => WIDGETS["applebatt"].draw(), intervalLow);
-
+  reload()
+  Bangle.on('charging',function(charging) { draw(); });
+  id = setInterval(()=>WIDGETS["applebatt"].draw(), intervalLow);
+  
+  WIDGETS["applebatt"]={area:"tr",width:32,draw:draw, reload:reload};
 })();
