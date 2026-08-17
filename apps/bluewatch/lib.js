2f605005a421adefeebf372659e55cecedc1b3f2
RKBoss6 <kabironkar@gmail.com>
2026-08-16 22:58:07 -0400

    Add calories support

--- apps/bluewatch/lib.js
+++ apps/bluewatch/lib.js
@@ -93,6 +93,10 @@ function sendRawHealthData(data) {
     sendableData.movement = data.movement;
     sendableData.state = data.movement <= 150 ? "sedentary" : "moving";
   }
+  if (global.calories) {
+    sendableData.activeCalories = global.calories.activeCaloriesBurned || 0;
+    sendableData.bmrCalories = global.calories.bmrCaloriesBurned || 0;
+  }
   if (data.bpmConfidence && data.bpm) {
     if (data.bpmConfidence > 75 && data.bpm != 0) sendableData.hr = data.bpm;
     sendableData.confidence = data.bpmConfidence;
