9a493a56f2a5b470d296a32f3000dd213f963149
RKBoss6 <kabironkar@gmail.com>
2026-08-30 17:21:53 -0400

    Add settings and screenshot

--- apps/gridlaunch/app.js
+++ apps/gridlaunch/app.js
@@ -1,24 +1,27 @@
-let launchCache = require("launch_utils").cache({showClocks:true,showLaunchers:true});
+
+let s = require("Storage");
+let settings = Object.assign(
+  {showClocks:false,showLaunchers:false, showWidgets:true},s.readJSON("gridlaunch.settings.json")||{});
+
+let launchCache = require("launch_utils").cache(settings);
 let apps = launchCache.apps;
 let chunkedApps = [];
 
 for (let i = 0; i < apps.length; i += 3) {
   chunkedApps.push(apps.slice(i, i + 3));
 }
-let s = require("Storage");
-print(chunkedApps)
-
-Bangle.loadWidgets()
-Bangle.drawWidgets()
+if(settings.showWidgets){
+  Bangle.loadWidgets();
+  Bangle.drawWidgets();
+}
 E.showScroller({
-  h : 70, 
+  h : 70,
   c : chunkedApps.length-1,
   draw : (idx, r) => {
     if (!chunkedApps[idx]) return;
 
-    const iconW = 43; 
+    const iconW = 43;
     const third = r.w / 3;
-    
     const x0 = r.x + (third * 0.5) - (iconW / 2);
     const x1 = r.x + (third * 1.5) - (iconW / 2);
     const x2 = r.x + (third * 2.5) - (iconW / 2);
@@ -36,21 +39,22 @@ E.showScroller({
   },
   select : (idx, touch) => {
     let hIdx = 2;
-    if(touch.x<g.getWidth()/3*2) hIdx = 1
-    if(touch.x<g.getWidth()/3) hIdx = 0
+    if(touch.x<g.getWidth()/3*2) hIdx = 1;
+    if(touch.x<g.getWidth()/3) hIdx = 0;
 
     if (Bangle.haptic) Bangle.haptic("touch");
      var app = chunkedApps[idx][hIdx];
         if (!app) return;
         if (Bangle.haptic) Bangle.haptic("touch");
-        if (!app.src || require("Storage").read(app.src)===undefined) {
+        if (!app.src || s.read(app.src)===undefined) {
           E.showScroller();
           E.showMessage(/*LANG*/"App Source\nNot found");
           setTimeout(drawMenu, 2000);
         } else {
           require("launch_utils").loadApp(app);
       }
-  }
+  },
+  back : Bangle.showClock,
 });
 
 
