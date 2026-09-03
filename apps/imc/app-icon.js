80e0972a907d9b8b2c57992776eb50744a8df590
RKBoss6 <kabironkar@gmail.com>
2026-09-02 21:52:29 -0400

    fix icon not being visible in launchers

--- apps/imc/app-icon.js
+++ apps/imc/app-icon.js
@@ -1,1 +1,1 @@
-require("heatshrink").decompress(atob("lEowMB/4A/8+H+H+h/HAgP4gYEB8HwofHwfg8PzAgXj/4EB4AECwPIAgWJAgeIxAEDzAECxkY8fxAgM4+IED+A2Bx04/EHJIcfQ3YA=="))
+require("heatshrink").decompress(atob("lEowJC/AAMYvEPgPwnAEBg/8AgMfh+8nF8j8ehgECjkAAgM/AgV+m4ECuwEDu93AgdnAgVz88chwEB48OAgcPGwNx48H/BJD8CG7"))
